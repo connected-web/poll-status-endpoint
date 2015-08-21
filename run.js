e29c02c1eda1c998982e956d9e6c06441f3e4092
John Beech <johnbeech@users.noreply.github.com>
2015-08-21 14:27:10 +0100

    Create run.js

---
+++ run.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var assert = require('assert');
+
+var testUrl = process.argv[2] || 'No url set';
+var expectedStatusBody = 'Status 200 OK';
+
+// Try for 2 minutes, every 10 seconds, to see if server has booted up
+runTest(12);
+
+function runTest(attempts) {
+  var retryDelayMs = 10000;
+  if(attempts <= 0) {
+    console.log('To many failed attempts, gave up.');
+    process.exit(1);
+  }
+  request.get(testUrl, function(error, response, body) {
+
+      if(body) {
+        console.log('Body', body);
+        try {
+          assert.equal(expectedStatusBody, body);
+
+          console.log('Status Endpoint OK');
+          process.exit(0);
+        }
+        catch(exception) {
+          console.log('Failed attempt: ', error, attempts, 'remaining');
+          setTimeout(runTest, retryDelayMs, attempts - 1);
+        }
+      }
+      else {
+        console.log('Error', error);
+        console.log('Failed attempt: ', error, attempts, 'remaining');
+        setTimeout(runTest, retryDelayMs, attempts - 1);
+      }
+  });
+}
