f0029c554dd2f09a428bb97265f53a561f2908c1
John Beech <john.beech@bbc.co.uk>
2015-08-21 14:49:19 +0100

    Changed equality assertion to regex matcher.

--- run.js
+++ run.js
@@ -1,38 +1,38 @@
 var request = require('request');
 var assert = require('assert');
 
-var testUrl = process.argv[2] || 'No url set';
-var expectedStatusBody = process.argv[3] || 'Status 200 OK';
+var testUrl = process.argv[2] || 'http://no-url-set';
+var expectedMatcher = process.argv[3] || 'Status 200 OK';
 
 // Try for 2 minutes, every 10 seconds, to see if server has booted up
 runTest(12);
 
 function runTest(attempts) {
-  var retryDelayMs = 10000;
-  if(attempts <= 0) {
-    console.log('To many failed attempts, gave up.');
-    process.exit(1);
-  }
-  request.get(testUrl, function(error, response, body) {
+    var retryDelayMs = 10000;
+    if (attempts <= 0) {
+        console.log('To many failed attempts, gave up.');
+        process.exit(1);
+    }
+    request.get(testUrl, function(error, response, body) {
 
-      if(body) {
-        console.log('Body', body);
-        try {
-          assert.equal(expectedStatusBody, body);
+        if (body) {
+            console.log('Body', body);
+            try {
+                if (!body.match(expectedMatcher)) {
+                    assert.fail('Response body did not contain "' + expectedMatcher + '"');
+                }
 
-          console.log('Status Endpoint OK');
-          process.exit(0);
+                console.log('Status Endpoint OK');
+                process.exit(0);
+            } catch (exception) {
+                var message = (exception) ? exception.message : exception;
+                console.log('Failed attempt:', message, attempts, 'remaining');
+                setTimeout(runTest, retryDelayMs, attempts - 1);
+            }
+        } else {
+            console.log('Error', error);
+            console.log('Failed attempt:', error, attempts, 'remaining');
+            setTimeout(runTest, retryDelayMs, attempts - 1);
         }
-        catch(exception) {
-          var message = (exception) ? exception.message : exception;
-          console.log('Failed attempt: ', message, attempts, 'remaining');
-          setTimeout(runTest, retryDelayMs, attempts - 1);
-        }
-      }
-      else {
-        console.log('Error', error);
-        console.log('Failed attempt: ', error, attempts, 'remaining');
-        setTimeout(runTest, retryDelayMs, attempts - 1);
-      }
-  });
+    });
 }
